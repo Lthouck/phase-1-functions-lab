4790dbfbb5b91d0b126cadb05a713aeccc9b03f6
Lesslie Houck <Lesslie00@outlook.com>
2022-08-08 15:43:25 -0700

    commit tree ba7ff434b9144de8a91403a792d028d255165fa8

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock) {
+    return Math.abs(42 - pickupBlock)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function distanceFromHqInBlocks(block) {
+//   const hq = 42
+//   if (block >= hq) {
+//     return block - 42
+//   } else {
+//     return 42 - block
+//   }
+  
+// }
+
+// function distanceFromHqInFeet(block) {
+//     return (distanceFromHqInBlocks(block) * 264)
+
+// }
+
+// function distanceTravelledInFeet(start, end){
+//     if (start < end) {
+//         return (end - start) * 264
+//     } else {
+//         return (start - end) * 264
+//     }
+// }
+
+
+// function calculatesFarePrice(start, end) {
+//     const distance = distanceTravelledInFeet(start, end)
+//     let fare = 0
+//     if (distance > 2500) {
+//         return 'cannot travel that far'
+//     } 
+//     if (distance > 400 && distance <= 2000 ) {
+//         fare += (distance - 400) * .02
+//     } else if (distance > 2000 && distance <= 2500) {
+//         fare += 25
+//     }
+//         return fare
+// }
+
